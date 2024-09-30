066ac496c06e90ca2264b8275d072472e2a720e6
Nico Cantrell <nico@mocamail.com>
2024-09-29 19:18:54 -0700

    added strafe right to main menu

--- Assets/Scripts/Level Scripts/MainMenu.cs
+++ Assets/Scripts/Level Scripts/MainMenu.cs
@@ -9,4 +9,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Airstrafe");
     }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 }
